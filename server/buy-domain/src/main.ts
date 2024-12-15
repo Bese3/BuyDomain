99570c5db61154a43965994b1abaf090a78d0e56
Besufikad Yilma <111793600+Bese3@users.noreply.github.com>
2024-12-15 16:06:42 +0300

    Update main.ts

--- server/buy-domain/src/main.ts
+++ server/buy-domain/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://domainmaster.danielyilma.tech',
+    origin: 'https://domain-master.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
